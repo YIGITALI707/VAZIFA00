525292daee64bc032af2f1d55a2c5603150ab3a6
YIGITALI707 <alibek1998ali12345@gmail.com>
2024-10-12 07:33:11 +0300

    first commit

---
+++ main/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.title}"
+
